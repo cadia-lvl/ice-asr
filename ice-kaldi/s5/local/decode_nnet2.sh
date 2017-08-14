01a5b2c55a19ac9effaab75ae8968d778c65ae2e
Anna Nikulasdottir <anna.b.nik@gmx.de>
2017-08-14 10:22:50 +0000

    deleted obsolete files

--- ice-kaldi/s5/local/decode_nnet2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. cmd.sh
-
-steps/nnet2/decode.sh --nj 2 --cmd "$decode_cmd" --online-ivector-dir models/nnet2/ivectors_test_data models/nnet2/graph data/test_data_hires models/nnet2/decode_test
-
-local/score.sh data/test_data_hires models/nnet2/graph models/nnet2/decode_test
